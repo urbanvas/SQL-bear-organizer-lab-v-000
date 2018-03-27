27134a8c03c481463c2fe34751f34bfd4b87d386
Neil Seejoor <nvseejoor@gmail.com>
2018-03-27 16:51:12 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
-  id PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender TEXT,
+  gender CHAR(1),
   color TEXT,
   temperament TEXT,
-  alive INTEGER
+  alive BOOLEAN
 );
